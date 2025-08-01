11ab51d866597cb2c330296899feb7261378c32a
Amarnathkk <amarnathk.akk@gmail.com>
2025-08-01 18:07:51 +0530

    Implemented a Hamburger menu for mobile view. Worked on making the Navbar and Topbar responsive.

--- src/app/components/HomePage/HeroSection.tsx
+++ src/app/components/HomePage/HeroSection.tsx
@@ -3,35 +3,25 @@ import Navbar from './Navbar'
 
 function HeroSection() {
     return (
-        <div className="w-full h-screen bg-cover"
+        <div className="w-full h-[800px] md:h-[1000px] lg:h-[1200px] bg-cover bg-center"
             style={{ backgroundImage: "url('/assets/Heroassets/Banner 1.png')" }}
         >
 
             <Navbar />
 
-
-            <div className='text-white flex justify-center pt-35 font-bold text-5xl'>
-
+            <div className='text-white flex justify-center px-4 md:px-14 lg:px-0 pt-2 md:pt-14 lg:pt-35 font-bold text-xl md:text-4xl lg:text-5xl text-center'>
                 <h1>Building Smart Solutions for a Digital-First Future</h1>
-
-
             </div>
 
-
-            <div className='text-white flex justify-center pt-4'>
-                <p>We craft scalable and secure digital products that solve real business challenges.</p>
+            <div className='text-white flex justify-center px-5 md:px-12 lg:px-0 pt-4 text-center'>
+                <p className="text-sm md:text-base lg:text-base">We craft scalable and secure digital products that solve real business challenges.</p>
             </div>
 
-
-
-            <div className="flex justify-center mt-10">
-                <button className="bg-gradient-to-r from-orange-500 to-purple-600 hover:from-orange-600 hover:to-purple-700 text-white font-semibold px-6 py-3 rounded-2xl shadow-lg cursor-pointer">
+            <div className="flex justify-center mt-6 md:mt-8 lg:mt-8 px-4">
+                <button className="bg-gradient-to-r from-orange-500 to-purple-600 hover:from-orange-600 hover:to-purple-700 text-white font-semibold px-6 py-3 md:px-8 md:py-4 lg:px-6 lg:py-3 rounded-2xl shadow-lg cursor-pointer text-sm md:text-base lg:text-base transition-all duration-300">
                     Connect With Us
                 </button>
             </div>
-
-
-
 
         </div>
     )
--- src/app/components/HomePage/Navbar.tsx
+++ src/app/components/HomePage/Navbar.tsx
@@ -1,8 +1,16 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 
 function Navbar() {
+    const [hamburgerlist, sethamburgerlist] = useState<boolean>(false);
+
+    const handlehamburger = () => {
+        sethamburgerlist((prev) => !prev);
+    };
+
     return (
-        <div className="sticky top-0 z-50 flex justify-between items-center px-34 py-4 text-white ">
+        <div className="sticky top-0 z-50 flex gap-10 justify-between items-center px-6 md:px-18 py-4 text-white">
 
 
             {/* Logo */}
@@ -12,17 +20,50 @@ function Navbar() {
                     alt="Softworks Logo"
                     className="h-10 w-auto"
                 />
-
             </div>
 
 
-            {/* Navigation Menu */}
-            <div className="flex gap-12 text-sm font-semibold items-center pr-120">
-                <p className="cursor-pointer ">Home</p>
-                <p className="cursor-pointer ">Services</p>
-                <p className="cursor-pointer ">About Us</p>
-                <p className="cursor-pointer ">Careers</p>
-                <p className="cursor-pointer ">Contacts</p>
+            {/* Menu for md (tablet) and above */}
+            <div className="hidden md:flex gap-10 text-sm font-semibold items-center mx-auto">
+
+                <p className="cursor-pointer">Home</p>
+                <p className="cursor-pointer">Services</p>
+                <p className="cursor-pointer">About Us</p>
+                <p className="cursor-pointer">Careers</p>
+                <p className="cursor-pointer">Contacts</p>
+            </div>
+
+
+
+            {/* Hamburger for mobile (below md) */}
+            <div className="md:hidden">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="size-8 cursor-pointer"
+                    onClick={handlehamburger}
+                >
+                    <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                    />
+                </svg>
+
+                {hamburgerlist && (
+                    <div className="absolute top-16 left-0 right-0 bg-white/10 backdrop-blur-lg text-white px-6 py-4 border-t z-40 rounded-b-xl shadow-xl border border-white/10">
+                        <div className="flex flex-col divide-y divide-white/20 overflow-hidden rounded-lg">
+                            <p className="cursor-pointer py-3 hover:bg-white/10 transition">Home</p>
+                            <p className="cursor-pointer py-3 hover:bg-white/10 transition">Services</p>
+                            <p className="cursor-pointer py-3 hover:bg-white/10 transition">About Us</p>
+                            <p className="cursor-pointer py-3 hover:bg-white/10 transition">Careers</p>
+                            <p className="cursor-pointer py-3 hover:bg-white/10 transition">Contacts</p>
+                        </div>
+                    </div>
+                )}
             </div>
         </div>
     );
