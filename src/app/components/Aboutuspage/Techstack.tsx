9139970683b9073e04b6de17fd5d54917349a61e
Amarnathkk <amarnathk.akk@gmail.com>
2025-08-13 13:35:32 +0530

    Made Aboutus page fully responsive

--- src/app/components/Aboutuspage/Techstack.tsx
+++ src/app/components/Aboutuspage/Techstack.tsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 function Techstack() {
   return (
-    <div className='mt-8 px-28'>
+    <div className='mt-8 px-4 md:px-16 lg:px-24 xl:px-28'>
 
       <div className='flex flex-col gap-2 items-center'>
-        <h1 className='text-3xl font-bold'>Our Tech Stack</h1>
-        <p className='text-gray-600'>Your Developers and Our Integration</p>
+        <h1 className="text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-3xl font-bold">Our Tech Stack</h1>
+        <p className='ext-sm sm:text-base md:text-md lg:text-lg xl:text-base text-gray-600'>Your Developers and Our Integration</p>
 
       </div>
 
@@ -14,7 +14,7 @@ function Techstack() {
 
       <div className='flex justify-center items-center mt-10'>
 
-        <img src="/assets/Aboutus/Techstackassets/2.png" className='h-[600px] w-auto' alt="" />
+        <img src="/assets/Aboutus/Techstackassets/2.png" className='h-[300px] md:h-[500px] lg:h-[550px] xl:h-[600px] w-auto' alt="" />
 
 
       </div>
